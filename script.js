e31af997dbc50c797d09c243f8eaef86bb5a2139
Dariusz Franczak <darius32@op.pl>
2018-12-06 23:10:49 +0000

    completed

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ new Vue({
   data: {
     heading: 'Helo World',
     paragraph: 'Wecome to the Vue.js'
+  },
+  methods: {
+    toUpperCase: function (val) {
+      return val.toUpperCase();
+    }
   }
 });
+
+/*
+Wszystkie metody definiujemy w obiekcie methods jako kolejny obiekt z funkcja jako wartosc
+*/
