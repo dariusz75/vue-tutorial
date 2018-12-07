412fc2138f81101033860bc1b197fb59c95c3d5c
Dariusz Franczak <darius32@op.pl>
2018-12-07 21:47:04 +0000

    06 completed

--- script.js
+++ script.js
@@ -1,39 +1,23 @@
 new Vue({
   el: '#app',
   data: {
-    heading: 'Helo World',
-    paragraph: 'Wecome to the Vue.js',
-    firstName: 'John',
-    lastName: 'Smith',
-    url1: 'https://www.bbc.co.uk/',
-    url2: 'https://www.dailymail.co.uk/home/index.html'
+    heading: 'Events',
+    paragraph: 'Counter: ',
+    counter: 0
   },
   methods: {
-    toUpperCase: function (val) {
-      return val.toUpperCase();
+    increment: function () {
+      this.counter++;
     },
-    bolder: function(val) {
-      return '<strong>' + val + '</strong>';
-    }
-  },
-  computed: {
-    fullName: function () {
-      return this.firstName + ' ' + this.lastName;
+    decrement: function () {
+      this.counter--;
+    },
+    reset: function () {
+      this.counter = 0;
     }
   }
 });
 
 /*
-Aby dynamicznie wstawic do elementu DOM atrybut musimy zadeklarowac go w obiekcie data 
-a naastepnie w tagu HTML uzyc dyrektywy v-bind:href lub :href 
-jak ponizej (oraz w pliku index.html)
 
-<ul>
-        <li>
-          <a v-bind:href="url1">{{ url1 }}</a>
-        </li>
-        <li>
-          <a :href="url2">{{ url2 }}</a>
-        </li>
-      </ul>
 */
