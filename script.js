82f96e8247a8a5da772202b2de7f42d3b0b97036
Dariusz Franczak <darius32@op.pl>
2018-12-08 00:28:23 +0000

    08 completed

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 new Vue({
   el: '#app',
   data: {
-    heading: 'Events',
-    paragraph: 'Counter: ',
-    counter: 0
+    heading: 'v-for directive',
+    counter: 0,
+    users: [
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz"
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv"
+      },
+      {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net"
+      },
+      {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org"
+      },
+      {
+        id: 5,
+        name: "Chelsey Dietrich",
+        username: "Kamren",
+        email: "Lucio_Hettinger@annie.ca"
+      }
+    ]
   },
   methods: {
-    increment: function () {
-      this.counter++;
-    },
-    decrement: function () {
-      this.counter--;
-    },
-    reset: function () {
-      this.counter = 0;
+    showTable: function () {
+
     }
   }
 });
@@ -21,3 +46,5 @@ new Vue({
 /*
 
 */
+
+
