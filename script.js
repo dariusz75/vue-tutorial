8ac772633ef2dbfec2170054bad1c264602e1227
Dariusz Franczak <darius32@op.pl>
2018-12-08 17:53:15 +0000

    project completed

--- script.js
+++ script.js
@@ -1,55 +1,38 @@
 new Vue({
   el: '#app',
   data: {
-    heading: 'v-for directive',
-    counter: 0,
-    users: [
-      {
-        id: 1,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz"
-      },
-      {
-        id: 2,
-        name: "Ervin Howell",
-        username: "Antonette",
-        email: "Shanna@melissa.tv"
-      },
-      {
-        id: 3,
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net"
-      },
-      {
-        id: 4,
-        name: "Patricia Lebsack",
-        username: "Karianne",
-        email: "Julianne.OConner@kory.org"
-      },
-      {
-        id: 5,
-        name: "Chelsey Dietrich",
-        username: "Kamren",
-        email: "Lucio_Hettinger@annie.ca"
-      }
-    ]
+    heading: 'Counter',
+    counter: 10,
+    inProgress: false
   },
   methods: {
-    remove: function (index) {
-      this.users.splice(index, 1);
+    start: function () {
+      this.inProgress = true;
+      this.countdown();
+    },
+    increment: function () {
+      this.counter++;
+    },
+    decrement: function () {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    },
+    countdown: function () {
+      this.counter--;
+
+      if (this.counter > 0) {
+        setTimeout(this.countdown, 1000);
+      } else {
+        this.counter = 10;
+        this.inProgress = false;
+      }
     }
   }
 });
 
 /*
-metoda remove to funkcja do ktorej przekazujemy argument w postaci numeru indexu urzytkownika
-przekazany z elementu row tabeli, podczas wykonywanie eventu click. 
-Patrz plik index.html
-<tr v-for="(user, index) in users" @click="remove(index)">
 
-Nastepnie funkcja metody remove wycina element tablicy users o podanym indexie.
 */
 
 
